4203d691a911075649aefd0530f5c1452ba8f492
Ceejayski <sirchijioke1@gmail.com>
2021-11-20 09:30:03 +0100

    Move recentlyViewed component to component folder

--- src/template/index.js
+++ src/template/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Container } from 'react-bootstrap';
 import NavBar from '../components/Navbar';
 import Footer from '../components/footer';
-import RecentlyViewed from '../recentlyViewed';
+import RecentlyViewed from '../components/recentlyViewed';
 
 export default function Template(props) {
   const { children } = props;
